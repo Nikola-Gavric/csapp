eb2edd3ba8c409b312ac5b67ab9a18a9a26d52d2
RomanShupikov97 <webmobile97@gmail.com>
2021-04-09 11:30:19 +0800

    update code for c

--- csapp.c
+++ csapp.c
@@ -23,18 +23,22 @@
  **************************/
 /* $begin errorfuns */
 /* $begin unixerror */
+
 void unix_error(char *msg) /* Unix-style error */
 {
     fprintf(stderr, "%s: %s\n", msg, strerror(errno));
     exit(0);
 }
+
 /* $end unixerror */
 
 void posix_error(int code, char *msg) /* Posix-style error */
+
 {
     fprintf(stderr, "%s: %s\n", msg, strerror(code));
     exit(0);
 }
+
 
 void gai_error(int code, char *msg) /* Getaddrinfo-style error */
 {
